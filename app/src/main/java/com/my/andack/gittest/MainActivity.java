c4159fbee6a16f49d9ad3914ef75a1b1f22e6cec
anDack <1160083806@qq.com>
2017-01-04 13:15:48 +0800

    修改文件

--- app/src/main/java/com/my/andack/gittest/MainActivity.java
+++ app/src/main/java/com/my/andack/gittest/MainActivity.java
@@ -1,13 +1,55 @@
 package com.my.andack.gittest;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText sender;
+    private EditText content;
+    private Button setBtn;
+    //    private SharePreferenceTools tools;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+//        tools=new SharePreferenceTools(MainActivity.this);
+
+        setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setInfo();
+            }
+        });
+
+    }
+
+    private void setInfo() {
+        String senderT=sender.getText().toString();
+        String contentT=content.getText().toString();
+        if (!senderT.equals("")&&!contentT.equals(""))
+        {
+            //在这里写入数据
+            SharedPreferences.Editor editor=getSharedPreferences("user",MODE_PRIVATE).edit();
+            editor.putString("name",senderT);
+            editor.putString("content",contentT);
+
+        }else
+        {
+            Toast.makeText(this,R.string.note,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initView() {
+        sender= (EditText) findViewById(R.id.theSender);
+        content= (EditText) findViewById(R.id.theContent);
+        setBtn= (Button) findViewById(R.id.setBtn);
     }
 }
